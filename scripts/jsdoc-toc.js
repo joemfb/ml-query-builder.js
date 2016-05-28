128bb101fd05778d6446ca5314f3d8f53b700685
Joseph Bryan <joemfb@gmail.com>
2016-05-27 22:00:26 -0400

    Updates

---
+++ scripts/jsdoc-toc.js
@@ -0,0 +1,17 @@
+(function($) {
+    // TODO: make the node ID configurable
+    var treeNode = $('#jsdoc-toc-nav');
+
+    // initialize the tree
+    treeNode.tree({
+        autoEscape: false,
+        closedIcon: '&#x21e2;',
+        data: [{"label":"<a href=\"module-extensions.html\">extensions</a>","id":"module:extensions","children":[]},{"label":"<a href=\"module-queryBuilder.html\">queryBuilder</a>","id":"module:queryBuilder","children":[]}],
+        openedIcon: ' &#x21e3;',
+        saveState: true,
+        useContextMenu: false
+    });
+
+    // add event handlers
+    // TODO
+})(jQuery);
